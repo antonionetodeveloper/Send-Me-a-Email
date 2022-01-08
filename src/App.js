83aa2a5bbaebaee0406cbec76ceb43093cf59f1c
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-08 14:49:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App(){
           onChange={(e1) => {setWho(e1.target.value)}}
         />
  
-        <label/>
+        <label htmlFor="Content"/>
         <textarea 
           className="content" 
           name="message"
